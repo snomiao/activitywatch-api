75b102aa9e566bf206ab012df889387bbc7f0946
snomiao <snomiao@gmail.com>
2024-06-26 18:43:09 +0900

    feat(main): use with credentials
    use with credentials

--- index.ts
+++ index.ts
@@ -1,3 +1,29 @@
-import { ActivityWatchAPI } from "./Api";
+import { ActivityWatchAPI, type ApiConfig } from "./Api";
 export * from "./Api";
 export default ActivityWatchAPI;
+
+export function AWApi<Options extends Omit<ApiConfig, "baseUrl">>(
+  baseUrl: string,
+  options?: Options
+) {
+  const { username, password } = new URL(baseUrl);
+
+  // remove username and password from url
+  const url = new URL(baseUrl);
+  url.username = "";
+  url.password = "";
+  baseUrl = url.href;
+
+  // fetch with basic auth
+  return new ActivityWatchAPI({
+    baseUrl,
+    ...options,
+    baseApiParams: {
+      ...options?.baseApiParams,
+      headers: {
+        Authorization: `Basic ${btoa(`${username}:${password}`)}`,
+        ...options?.baseApiParams?.headers,
+      },
+    },
+  }).v0;
+}
